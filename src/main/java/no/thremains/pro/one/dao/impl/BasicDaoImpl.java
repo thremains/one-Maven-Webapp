5eda8df882b365e83d3db7ad2ef1ff64e1929587
seanWg <suchenwon@gmail.com>
2016-01-12 19:34:04 +0800

    继续完善框架搭建

--- src/main/java/no/thremains/pro/one/dao/impl/BasicDaoImpl.java
+++ src/main/java/no/thremains/pro/one/dao/impl/BasicDaoImpl.java
@@ -1,7 +1,10 @@
 package no.thremains.pro.one.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import no.thremains.pro.one.dao.BasicDao;
 
+@Repository
 public class BasicDaoImpl implements BasicDao {
 
 }
---
+++ src/main/java/no/thremains/pro/one/service/impl/BasicServiceImpl.java
@@ -0,0 +1,10 @@
+package no.thremains.pro.one.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import no.thremains.pro.one.service.BasicService;
+
+@Service
+public class BasicServiceImpl implements BasicService {
+
+}
